fdc9577d74e53a3ccf1c42cb4b9922ff7084d6b4
heitorl <juniorio01@hotmail.com>
2024-05-12 18:24:56 -0300

    add cors3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import express from "express";
+import cors from "cors";
 import { buscarLojasPorBairro } from "./request.js";
 
 const app = express();
 const port = 3333;
 app.use(express.json());
 
+
+
+app.use(cors());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.get('/lojas', async (req, res) => {
     const cidade = req.query.cidade;
